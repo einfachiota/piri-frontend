9f5d7823fc581513e1bf9e1f5bf0a222aef91463
huhn <sehe89@gmail.com>
2019-04-11 19:03:42 +0200

    add table with nodes and element ui

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,76 @@
 <template>
   <div class="home">
-
+    <h1>{{url}}</h1>
+    <el-table
+      :data="nodes"
+      style="width: 100%"
+      :row-class-name="tableRowClassName"
+        :default-sort = "{prop: 'count', order: 'descending'}">
+      <el-table-column
+        prop="name"
+        label="Name">
+      </el-table-column>
+      <el-table-column
+        prop="count"
+        label="Jobs"
+        sortable>
+      </el-table-column>
+      <el-table-column
+        label="Version"
+        sortable>
+        <template slot-scope="scope">
+          {{ scope.row.appName }} {{ scope.row.appVersion }}
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data() {
+    return {
+      url: 'https://nutzdoch.einfachiota.de/nodes',
+      nodes: []
+    }
+  },
+  methods: {
+    fetchData() {
+      let self = this;
+      fetch(this.url)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(nodes) {
+          console.log(nodes);
+          if(nodes) {
+            self.nodes = nodes
+          }
+        });
+    },
+    tableRowClassName({row, rowIndex}) {
+        if (row.available && row.delay < 4) {
+          return 'success-row';
+        } else {
+          return 'danger-row';
+        }
+        return '';
+      }
+  },
+  created() {
+    this.fetchData();
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+  .el-table .danger-row {
+    background: oldlace;
+  }
 
+  .el-table .success-row {
+    background: #f0f9eb;
+  }
 </style>
