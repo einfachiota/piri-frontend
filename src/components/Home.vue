1053c8fa4e801f496073624226cb39ea21e20372
huhn <sehe89@gmail.com>
2019-04-15 18:32:16 +0200

    add status bar component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="home">
     <h1>{{url}}</h1>
+    <StatusBar />
     <NodeList />
+
   </div>
 </template>
 
 <script>
+import StatusBar from './StatusBar'
 import NodeList from './NodeList'
+
 export default {
   name: "Home",
-  components: { NodeList },
+  components: { StatusBar, NodeList },
   data() {
     return {
       url: "https://nutzdoch.einfachiota.de/",
---
+++ src/components/StatusBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-tag type="success">Online</el-tag>
+</template>
+
+<script>
+export default {
+  name: "StatusBar",
+  data() {
+    return {
+      loading: false,
+      url: "https://nutzdoch.einfachiota.de/actuator/health"
+    };
+  },
+  methods: {
+    fetchData() {
+      let self = this;
+      fetch(this.url)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(status) {
+          if (status) {
+            console.log("status", status);
+          }
+        });
+    }
+  },
+  created() {
+    this.loading = true;
+    this.fetchData();
+    this.intervalid1 = setInterval(
+      function() {
+        this.fetchData();
+      }.bind(this),
+      5000
+    );
+  }
+};
+</script>
