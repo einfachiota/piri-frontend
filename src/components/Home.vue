bebb62c18cf106fae1baefed52e43a4419b8c98e
huhn <sehe89@gmail.com>
2019-04-15 18:12:40 +0200

    add loading spinner + auto refresh after 5 seconds

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,38 +1,30 @@
 <template>
-  <div class="home">
+  <div class="home" v-loading="loading">
     <h1>{{url}}</h1>
     <el-table
       :data="nodes"
       style="width: 100%"
       :row-class-name="tableRowClassName"
-        :default-sort = "{prop: 'count', order: 'descending'}">
-      <el-table-column
-        prop="name"
-        label="Name">
+      :default-sort="{prop: 'count', order: 'descending'}"
+    >
+      <el-table-column prop="name" label="Name"></el-table-column>
+      <el-table-column label="Version">
+        <template slot-scope="scope">{{ scope.row.appName }} {{ scope.row.appVersion }}</template>
       </el-table-column>
-      <el-table-column
-        label="Version">
-        <template slot-scope="scope">
-          {{ scope.row.appName }} {{ scope.row.appVersion }}
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="count"
-        label="Jobs"
-        sortable>
-      </el-table-column>
+      <el-table-column prop="count" label="Jobs" sortable></el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
-      url: 'https://nutzdoch.einfachiota.de/nodes',
+      loading: false,
+      url: "https://nutzdoch.einfachiota.de/nodes",
       nodes: []
-    }
+    };
   },
   methods: {
     fetchData() {
@@ -42,38 +34,41 @@ export default {
           return response.json();
         })
         .then(function(nodes) {
-          console.log(nodes);
-          if(nodes) {
-            self.nodes = nodes
+          if (nodes) {
+            self.nodes = nodes;
+            self.loading = false;
           }
         });
     },
-    tableRowClassName({row}) {
-        if (row.available && row.appName == 'POW Node') {
-          return 'success-row';
-        } 
-        if (row.available && row.delay < 4) {
-          return 'success-row';
-        } 
-        else {
-          return 'danger-row';
-        }
-        return '';
+    tableRowClassName({ row }) {
+      if (row.available && row.appName == "POW Node" || row.available && row.delay < 4) {
+        return "success-row";
+      } else {
+        return "danger-row";
       }
+    }
   },
   created() {
+    this.loading = true;
     this.fetchData();
+    this.intervalid1 = setInterval(
+      function() {
+        console.log("s")
+        this.fetchData();
+      }.bind(this),
+      5000
+    );
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-  .el-table .danger-row {
-    background: #ffb3b3;
-  }
+.el-table .danger-row {
+  background: #ffb3b3;
+}
 
-  .el-table .success-row {
-    background: #b3ffb3;
-  }
+.el-table .success-row {
+  background: #b3ffb3;
+}
 </style>
