6f9870661eb1cf0ca3fc7218ef94e815987ad444
huhn <sehe89@gmail.com>
2019-04-15 18:21:30 +0200

    create nodelist component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,74 +1,19 @@
 <template>
-  <div class="home" v-loading="loading">
+  <div class="home">
     <h1>{{url}}</h1>
-    <el-table
-      :data="nodes"
-      style="width: 100%"
-      :row-class-name="tableRowClassName"
-      :default-sort="{prop: 'count', order: 'descending'}"
-    >
-      <el-table-column prop="name" label="Name"></el-table-column>
-      <el-table-column label="Version">
-        <template slot-scope="scope">{{ scope.row.appName }} {{ scope.row.appVersion }}</template>
-      </el-table-column>
-      <el-table-column prop="count" label="Jobs" sortable></el-table-column>
-    </el-table>
+    <NodeList />
   </div>
 </template>
 
 <script>
+import NodeList from './NodeList'
 export default {
   name: "Home",
+  components: { NodeList },
   data() {
     return {
-      loading: false,
-      url: "https://nutzdoch.einfachiota.de/nodes",
-      nodes: []
-    };
-  },
-  methods: {
-    fetchData() {
-      let self = this;
-      fetch(this.url)
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(nodes) {
-          if (nodes) {
-            self.nodes = nodes;
-            self.loading = false;
-          }
-        });
-    },
-    tableRowClassName({ row }) {
-      if (row.available && row.appName == "POW Node" || row.available && row.delay < 4) {
-        return "success-row";
-      } else {
-        return "danger-row";
-      }
+      url: "https://nutzdoch.einfachiota.de/",
     }
-  },
-  created() {
-    this.loading = true;
-    this.fetchData();
-    this.intervalid1 = setInterval(
-      function() {
-        console.log("s")
-        this.fetchData();
-      }.bind(this),
-      5000
-    );
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-.el-table .danger-row {
-  background: #ffb3b3;
-}
-
-.el-table .success-row {
-  background: #b3ffb3;
-}
-</style>
---
+++ src/components/NodeList.vue
@@ -0,0 +1,75 @@
+<template>
+  <el-table
+    v-loading="loading"
+    :data="nodes"
+    style="width: 100%"
+    :row-class-name="tableRowClassName"
+    :default-sort="{prop: 'count', order: 'descending'}"
+  >
+    <el-table-column prop="name" label="Name"></el-table-column>
+    <el-table-column label="Version">
+      <template slot-scope="scope">{{ scope.row.appName }} {{ scope.row.appVersion }}</template>
+    </el-table-column>
+    <el-table-column prop="count" label="Jobs" sortable></el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: "NodeList",
+  data() {
+    return {
+      loading: false,
+      url: "https://nutzdoch.einfachiota.de/nodes",
+      nodes: []
+    };
+  },
+  methods: {
+    fetchData() {
+      let self = this;
+      fetch(this.url)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(nodes) {
+          if (nodes) {
+            self.nodes = nodes;
+            self.loading = false;
+          }
+        });
+    },
+    tableRowClassName({ row }) {
+      if (
+        (row.available && row.appName == "POW Node") ||
+        (row.available && row.delay < 4)
+      ) {
+        return "success-row";
+      } else {
+        return "danger-row";
+      }
+    }
+  },
+  created() {
+    this.loading = true;
+    this.fetchData();
+    this.intervalid1 = setInterval(
+      function() {
+        this.fetchData();
+      }.bind(this),
+      5000
+    );
+  }
+};
+</script>
+
+<style>
+
+.el-table .danger-row {
+  background: #ffb3b3;
+}
+
+.el-table .success-row {
+  background: #b3ffb3;
+}
+</style>
+
