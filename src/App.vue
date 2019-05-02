b9ddb4629f51f04dcd07f5b52806a25655843675
huhn <sehe89@gmail.com>
2019-05-02 13:13:24 +0200

    add layout and footer section

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <el-row>
-        <el-col :span="24">
-          <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal">
-            <el-menu-item index="1">
-              <router-link to="/" class="link-item">Home</router-link>
-            </el-menu-item>
-            <el-menu-item index="2">
-              <router-link to="/about" class="link-item">About</router-link>
-            </el-menu-item>
-          </el-menu>
-        </el-col>
-      </el-row>
-    </div>
-    <router-view/>
-  </div>
+  <el-container id="app">
+    <el-header>
+      <div id="nav">
+        <el-row>
+          <el-col :span="24">
+            <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal">
+              <el-menu-item index="1">
+                <router-link to="/" class="link-item">Home</router-link>
+              </el-menu-item>
+              <el-menu-item index="2">
+                <router-link to="/about" class="link-item">About</router-link>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </div>
+    </el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
 </template>
 
 <style>
