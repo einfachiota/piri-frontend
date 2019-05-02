a50e71111074153cefa82dd29f5812b00dbcf9fe
huhn <sehe89@gmail.com>
2019-05-02 13:04:36 +0200

    add nav

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <el-row>
+        <el-col :span="24">
+          <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal">
+            <el-menu-item index="1">
+              <router-link to="/" class="link-item">Home</router-link>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <router-link to="/about" class="link-item">About</router-link>
+            </el-menu-item>
+          </el-menu>
+        </el-col>
+      </el-row>
     </div>
     <router-view/>
   </div>
@@ -10,7 +20,7 @@
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -20,5 +30,19 @@
 
 .logo {
   height: 200px;
+}
+
+.el-menu-item {
+  overflow: hidden;
+  position: relative;
+  min-width: 100px;
+}
+.link-item {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  right: 0;
+  top: 0;
+  text-decoration: none;
 }
 </style>
