7b31f143e04c1be0f274454f52dd638cc9f2f0b1
huhn <sehe89@gmail.com>
2019-05-19 23:17:46 +0200

    add privacy and imprint

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Details from './views/Details.vue'
+import Imprint from './views/Imprint.vue'
+import Privacy from './views/Privacy.vue'
 
 Vue.use(Router)
 
@@ -25,6 +27,16 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/imprint',
+      name: 'imprint',
+      component: Imprint
+    },
+    {
+      path: '/privacy',
+      name: 'privacy',
+      component: Privacy
     }
   ]
 })
