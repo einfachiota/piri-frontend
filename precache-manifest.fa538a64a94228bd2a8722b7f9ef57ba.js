48a2133542fef88169c583cbe190bbd30d5c2eb8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-11 05:39:43 +0000

    Deploy einfachiota/pool.einfachIOTA.de to github.com/einfachiota/pool.einfachIOTA.de.git:gh-pages

---
+++ precache-manifest.fa538a64a94228bd2a8722b7f9ef57ba.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e672ec7df6061d2aea38",
+    "url": "/css/about.713bcdc3.css"
+  },
+  {
+    "revision": "e672ec7df6061d2aea38",
+    "url": "/js/about.617ef338.js"
+  },
+  {
+    "revision": "d32296be82b534c3c66c",
+    "url": "/css/app.744f09cb.css"
+  },
+  {
+    "revision": "d32296be82b534c3c66c",
+    "url": "/js/app.a4ce903b.js"
+  },
+  {
+    "revision": "0d2b47f67eac4e220928",
+    "url": "/css/chunk-vendors.6c51e417.css"
+  },
+  {
+    "revision": "0d2b47f67eac4e220928",
+    "url": "/js/chunk-vendors.ec01d1fb.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "ae303cc6d87717c5e1300078e6d59a8b",
+    "url": "/img/logo.ae303cc6.svg"
+  },
+  {
+    "revision": "166487f274654ecee99e9916051745df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0babfc3137e740a62e78175be743b14a",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
